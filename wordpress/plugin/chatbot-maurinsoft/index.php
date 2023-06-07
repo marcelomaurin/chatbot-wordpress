263434e981bb9be1f93dae8fd785d32835fe2b29
Marcelo Maurin <marcelo.maurin@comlink.com.br>
2023-06-07 11:10:02 -0300

    correcoes

--- wordpress/plugin/chatbot-maurinsoft/index.php
+++ wordpress/plugin/chatbot-maurinsoft/index.php
@@ -4,7 +4,7 @@ Plugin Name: Administrador do ChatBot da Maurinsoft
 Description: Administrar conteudo de dados do chatbot.
 Version: 1.0.1
 Author:  Marcelo Maurin Martins
-Author URI:  http://maurinsoft.com.br 
+Author URI:  http://maurinsoft.com.br
 Text Domain: maurinsoft
 */
 
@@ -78,13 +78,18 @@ function chat_admin_menu() {
   //add_submenu_page( 'chat-options', 'Chatbot - Base Operação', 'Base Operação', 'manage_options', 'chat_boperacao', 'wps_theme_func_boperacao');
   add_submenu_page( 'chat-options', 'Chatbot - Base Operação', 'Base Operação', 'manage_options', 'chat_boperacao', 'wps_theme_func_boperacao');
 
+
+  // Adiciona a rota do web service para chamar o arquivo ./ws/registra_log.php
+  add_action( 'rest_api_init', 'chat_register_api_routes' );
+  add_action( 'rest_api_init', 'registrar_chat_rota2_personalizada');
+
 }
 
 function wps_theme_func_hello(){
 ?>
     <h1><?php esc_html_e( 'Chat - Help', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
         echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div></div>';
 	include ("wellcome.php");
 }
@@ -93,7 +98,7 @@ function wps_theme_func_chatbot(){
 ?>
     <h1><?php esc_html_e( 'Chat - Simulador ', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
         echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
         <h2>Simulador do chatbot</h2></div>';
 	include ("chatbot.php");
@@ -104,14 +109,14 @@ function chat_basepergunta() {
     <h1><?php esc_html_e( 'Chat - Base Pergunta', 'chat-plugin-textdomain' ); ?></h1>
 
 <?php
-include "pagina.php"; 
+include "pagina.php";
 }
 
 function chat_historico(){
 ?>
     <h1><?php esc_html_e( 'Chat - Histórico', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
         echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>';
 		include ("historico.php");
 }
@@ -120,7 +125,7 @@ function wps_theme_func_bresposta(){
 ?>
     <h1><?php esc_html_e( 'Chat - Base Resposta', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
         echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>';
 	include ("bresposta.php");
 }
@@ -129,9 +134,9 @@ function wps_theme_func_pr(){
 	?>
     <h1><?php esc_html_e( 'Chat - Manager', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
         echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>';
-		include ("peresp.php");	
+		include ("peresp.php");
 }
 
 function wps_theme_func_boperacao(){
@@ -150,10 +155,10 @@ function funcchatbot(){
 ?>
     <h1><?php esc_html_e( 'Chat - Manager', 'chat-plugin-textdomain' ); ?></h1>
 
-<?php	
+<?php
 	echo("<div>Chat bot online</div>");
 	include ("chatbot.php");
-}	
+}
 
 
 
@@ -188,9 +193,154 @@ function load_chat_plugin_scripts( $hook ) {
 }
 
 
-function chat_rodape(){
-		echo "<center><a href='https://maurinsoft.com.br'>maurinsoft.com.br</a> </center>";
-	}
+function chat_rodape()
+{
+  echo "<center><a href='https://maurinsoft.com.br'>maurinsoft.com.br</a> </center>";
+}
 
+
+/*Funcoes de registro de ws*/
+function chat_registra_log_endpoint( WP_REST_Request $request ) {
+    $file_path = trailingslashit(ABSPATH) . 'ws/registra_log.php';
+    $response = wp_remote_post( get_site_url() . '/' . $file_path, array( 'timeout' => 120 ) );
+    return $response['body'];
+}
+
+
+function chat_register_api_routes() {
+    //...
+    register_rest_route( 'geiser/v1', '/registra_log', array(
+        'methods' => 'GET',
+        'callback' => 'geiser_registra_log_endpoint',
+        'permission_callback' => function () {
+            return current_user_can( 'manage_options' );
+        }
+    ) );
+}
+
+add_action('rest_api_init', 'registrar_chat_rota_personalizada');
+
+function registrar_chat_rota_personalizada() {
+  register_rest_route('chat/v1', '/registro.php', [
+    'methods' => 'GET',
+    'callback' => 'chat_lst_callback',
+  ]);
+  register_rest_route('chat/v1', '/runpy.php', [
+    'methods' => 'GET',
+    'callback' => 'chat_run_callback',
+  ]);
+}
+
+function chat_run_callback(WP_REST_Request $request)
+{
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'chathistorico';
+
+    // Retorne os resultados como uma resposta JSON
+    return new WP_REST_Response("OK", 200);
+}
+
+function chat_lst_callback(WP_REST_Request $request) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'chathistorico';
+
+    // Obtenha os parâmetros do corpo da solicitação
+    $data_inicio = $request->get_param('data_inicio');
+    $data_fim = $request->get_param('data_fim');
+    $ip = $request->get_param('ip');
+
+    // Construa a consulta SQL
+    $sql = "SELECT * FROM {$table_name}";
+
+    $where_clauses = array();
+    if (!empty($data_inicio)) {
+        $where_clauses[] = $wpdb->prepare('lastdt >= %s', $data_inicio);
+    }
+    if (!empty($data_fim)) {
+        $where_clauses[] = $wpdb->prepare('lastdt <= %s', $data_fim);
+    }
+    if (!empty($ip)) {
+        $where_clauses[] = $wpdb->prepare('ip = %s', $ip);
+    }
+
+    if (!empty($where_clauses)) {
+        $sql .= ' WHERE ' . implode(' AND ', $where_clauses);
+    }
+
+    $sql .= ' ORDER BY lastdt DESC';
+
+    // Execute a consulta e obtenha os resultados
+    $logs = $wpdb->get_results($sql);
+
+    // Retorne os resultados como uma resposta JSON
+    return new WP_REST_Response($logs, 200);
+}
+
+
+
+function registrar_chat_rota2_personalizada() {
+  register_rest_route('chat/v1', '/registro.php', [
+    'methods' => 'POST',
+    'callback' => 'chat_reg_callback',
+  ]);
+}
+
+function chat_reg_callback(WP_REST_Request $request) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'chathistorico';
+
+    //$usvh = $request->get_param('usvh');
+    //$temp = $request->get_param('temp');
+    //$hum = $request->get_param('hum');
+
+
+    // Obtenha o IP do cliente
+    $ip = $_SERVER['REMOTE_ADDR'];
+    //echo $ip;
+    //echo "<br/>";
+
+    /*
+
+    $sql = "SELECT * FROM {$wpdb->prefix}geiser_leitores WHERE token = '$ip'";
+    //echo $sql;
+    //echo "<br/>";
+
+    // Verificar se o dispositivo está cadastrado na tabela geiser_leitores
+    $device = $wpdb->get_row($sql);
+
+    //echo $device;
+    //echo "<br/>";
+
+    if ($usvh === null)
+    {
+        return new WP_REST_Response(array('error' => 'usvh nao definido'), 404);
+    }
+    if ($device === null) {
+        return new WP_REST_Response(array('error' => 'Device nao cadastrado'), 404);
+    }
+
+    // Obtenha a data e hora atual do sistema
+    $lastdt = date('Y-m-d H:i:s');
+
+    // O campo status deve ser sempre 1
+    $status = 1;
+
+    // Insira os dados na tabela
+    $wpdb->insert(
+        $table_name,
+        array(
+            'usvh' => $usvh,
+            'temp' => $temp,
+            'hum' => $hum,
+            'token' => $ip
+        ),
+        array('%s', '%s', '%s', '%s', '%d', '%d', '%s')
+    );
+    */
+
+    // Retorne uma resposta JSON com uma mensagem de sucesso
+    return new WP_REST_Response(array('message' => 'Registro inserido com sucesso!'), 200);
+}
 
 
